500e65ab0fbb952bbfe969ec705821979ffbb427
Noraiman04 <102566723+Noraiman04@users.noreply.github.com>
2022-04-19 11:05:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KIDS TOYS</title>
+</head>
+<body>
+	<center>
+		<!--Tiada huruf G-->
+		<h2>SENARAI BARANG MAINAN</h2>
+<body style = "background-color:#D77EF0;">
+   
+		
+		<table border="1" cellspacing="0" cellpadding="6">
+		  <tr bgcolor="#eee">	
+
+			<th>KOD PRODUK</th>
+			<th>NAMA PRODUK</th>
+        		<th>HARGA</th>
+        		<th>KUANT</th>
+
+<td>Hapus</td><!-- Isi tag <td> dengan Hapus-->
+			
+		  </tr>
+
+		  <?php   //fail config1.php tidak ada dalam folder
+		  	include 'config.php';
+
+		  	$papar=mysqli_query($connect,"SELECT * FROM jadualmainan");
+
+		  	while ($row=mysqli_fetch_array($papar)) {
+		  		$totalRecord=mysqli_num_rows($papar);
+		  		
+		  echo "
+		  <tr>	
+			<td>".$row['kod_produk']."</td>
+			<td>".$row['nama_produk']."</td>
+        		<td>".$row['harga']."</td>
+        		<td>".$row['kuantiti']."</td>
+<td><a href='delete.php?kod_produk=".$row['kod_produk']."' onclick='myHapus()'> Hapus </a></td>
+		  </tr>";
+		}
+		  ?>
+		</table>   <!--Tukar tambah.php ke add.php-->
+<p><a href="add.php">Tambah Rekod</a></p>
+<p><a href="kemaskini.php">Kemaskini</a></p>
+		
+	</center>
+
+	
+</body>
+</html>
+
+
